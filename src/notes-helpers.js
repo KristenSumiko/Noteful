51297e6eccc0c5c0eaa6c63fe6e90776c107b89a
Kristen Sumiko Jones <kristensumiko@gmail.com>
2020-12-17 22:48:16 -0500

    implemented design and routing

---
+++ src/notes-helpers.js
@@ -0,0 +1,14 @@
+export const findFolder = (folders=[], folderId) =>
+    folders.find(note => folders.id === folderId)
+
+export const findNote = (notes=[], noteId) =>
+    notes.find(note => note.id ===noteId)
+
+export const getNotesForFolder = (notes=[], folderId) => (
+    (!folderId)
+        ? notes
+        : notes.filter(note => note.folderId === folderId)
+)
+
+export const countNotesForFolder = (notes=[], folderId) =>
+    notes.filter(note => note.folderId === folderId).length
